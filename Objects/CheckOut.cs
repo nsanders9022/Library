3b8f21b63ac36652206251a8874d86df228db7a2
Alexandra Holcombe and Nicole Sanders <alexandra+nicole@>
2017-03-01 14:52:30 -0800

    Add all methods and tests for CheckOut Class

---
+++ Objects/CheckOut.cs
@@ -0,0 +1,201 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LibraryApp.Objects
+{
+    public class CheckOut
+    {
+        private int _id;
+        private DateTime _dueDate;
+        private DateTime _returnDate;
+        private int _patronsId;
+        private int _copiesId;
+
+        public CheckOut(DateTime dueDate, DateTime returnDate, int patronsId, int copiesId, int id = 0)
+        {
+            _id = id;
+            _dueDate = dueDate;
+            _returnDate = returnDate;
+            _patronsId = patronsId;
+            _copiesId = copiesId;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public DateTime GetDueDate()
+        {
+            return _dueDate;
+        }
+
+        public DateTime GetReturnDate()
+        {
+            return _returnDate;
+        }
+
+        public int GetPatronsId()
+        {
+            return _patronsId;
+        }
+
+        public int GetCopiesId()
+        {
+            return _copiesId;
+        }
+
+        //delete all rows from checkouts db table
+        public static void DeleteAll()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM checkouts;", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        //return list of all checkouts in the database
+        public static List<CheckOut> GetAll()
+        {
+            List<CheckOut> allCheckOuts = new List<CheckOut>{};
+
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM checkouts;", conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                int id = rdr.GetInt32(0);
+                DateTime dueDate = rdr.GetDateTime(1);
+                DateTime returnDate = rdr.GetDateTime(2);
+                int patronsId = rdr.GetInt32(3);
+                int copiesId = rdr.GetInt32(4);
+                CheckOut newCheckOut = new CheckOut(dueDate, returnDate, patronsId, copiesId, id);
+                allCheckOuts.Add(newCheckOut);
+            }
+
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+
+            if(conn != null)
+            {
+                conn.Close();
+            }
+            return allCheckOuts;
+        }
+
+        public override bool Equals(System.Object otherCheckOut)
+        {
+            if (!(otherCheckOut is CheckOut))
+            {
+                return false;
+            }
+            else
+            {
+                CheckOut newCheckOut = (CheckOut) otherCheckOut;
+                bool idEquality = (this.GetId() == newCheckOut.GetId());
+                bool dueDateEquality = (this.GetDueDate() == newCheckOut.GetDueDate());
+                bool returnDateEquality = (this.GetReturnDate() == newCheckOut.GetReturnDate());
+                bool patronsIdEquality = (this.GetPatronsId() == newCheckOut.GetPatronsId());
+                bool copiesIdEquality = (this.GetCopiesId() == newCheckOut.GetCopiesId());
+
+                return (idEquality && dueDateEquality && returnDateEquality && patronsIdEquality && copiesIdEquality);
+            }
+        }
+
+        //Save instance into database
+        public void Save()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO checkouts (due_date, date_returned, patrons_id, copies_id) OUTPUT INSERTED.id VALUES(@DueDate, @ReturnDate, @PatronsId, @CopiesId);", conn);
+
+            cmd.Parameters.Add(new SqlParameter("@DueDate", this.GetDueDate()));
+
+            cmd.Parameters.Add(new SqlParameter("@ReturnDate", this.GetReturnDate()));
+
+            cmd.Parameters.Add(new SqlParameter("@PatronsId", this.GetPatronsId()));
+
+            cmd.Parameters.Add(new SqlParameter("@CopiesId", this.GetCopiesId()));
+
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                this._id = rdr.GetInt32(0);
+            }
+
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+            if(conn != null)
+            {
+                conn.Close();
+            }
+        }
+
+        //return checkout information as per id argument
+        public static CheckOut Find(int id)
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM checkouts WHERE id = @CheckOutId;", conn);
+            cmd.Parameters.Add(new SqlParameter("@CheckOutId", id.ToString()));
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            int foundId = 0;
+            DateTime foundDueDate = new DateTime(1900, 1, 1);
+            DateTime foundReturnDate = new DateTime(1900, 1, 1);
+            int foundPatronsId = 0;
+            int foundCopiesId = 0;
+
+            while(rdr.Read())
+            {
+                foundId = rdr.GetInt32(0);
+                foundDueDate = rdr.GetDateTime(1);
+                foundReturnDate = rdr.GetDateTime(2);
+                foundPatronsId = rdr.GetInt32(3);
+                foundCopiesId = rdr.GetInt32(4);
+            }
+
+            CheckOut foundCheckOut = new CheckOut(foundDueDate, foundReturnDate, foundPatronsId, foundCopiesId, foundId);
+
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+
+            if(conn != null)
+            {
+                conn.Close();
+            }
+
+            return foundCheckOut;
+        }
+
+        //delete copy from db
+        public void DeleteCheckOut()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM checkouts WHERE id = @CheckOutId;", conn);
+            cmd.Parameters.Add(new SqlParameter("@CheckOutId", this.GetId()));
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+    }
+}
---
+++ Tests/CheckOutTest.cs
@@ -0,0 +1,146 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LibraryApp.Objects
+{
+    public class CheckOutTest: IDisposable
+    {
+        public CheckOutTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=library_test;Integrated Security=SSPI;";
+        }
+        //Test that if there are no cipies, GetAll returns empty
+        [Fact]
+        public void TestGetAll_NoCheckOuts_ReturnsEmptyList()
+        {
+            //Arrange, Act
+            CheckOut.DeleteAll();
+            List<CheckOut> allCheckOuts = CheckOut.GetAll();
+
+            //Assert
+            List<CheckOut> actualResult = allCheckOuts;
+            List<CheckOut> expectedResult = new List<CheckOut>{};
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        //test if equals override works
+        [Fact]
+        public void TestEqualOverride_TrueIfCheckOutIsSame()
+        {
+            //Arrange, Act
+            DateTime dueDate = new DateTime(2017, 3, 15);
+            DateTime returnDate = new DateTime(2017, 3, 7);
+            CheckOut firstCheckOut = new CheckOut(dueDate, returnDate, 1, 1);
+            CheckOut secondCheckOut = new CheckOut(dueDate, returnDate, 1, 1);
+
+            //Assert
+            Assert.Equal(firstCheckOut, secondCheckOut);
+        }
+
+        //tests if instances are saved to db
+        [Fact]
+        public void Test_Save_SavesToDatabase()
+        {
+            //Arrange
+            DateTime dueDate = new DateTime(2017, 3, 15);
+            DateTime returnDate = new DateTime(2017, 3, 7);
+            CheckOut newCheckOut = new CheckOut(dueDate, returnDate, 1, 1);
+
+            //Act
+            newCheckOut.Save();
+
+            //Assert
+            List<CheckOut> actualResult = CheckOut.GetAll();
+            List<CheckOut> expectedResult = new List<CheckOut>{newCheckOut};
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        //tests that each instance is assigned corresponding db id
+        [Fact]
+        public void TestSave_AssignIdtoObject()
+        {
+            //Arrange
+            DateTime dueDate = new DateTime(2017, 3, 15);
+            DateTime returnDate = new DateTime(2017, 3, 7);
+            CheckOut testCheckOut = new CheckOut(dueDate, returnDate, 1, 1);
+
+            //Act
+            testCheckOut.Save();
+            CheckOut savedCheckOut = CheckOut.GetAll()[0];
+
+            //Assert
+            int actualResult = savedCheckOut.GetId();
+            int expectedResult = testCheckOut.GetId();
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        //Tests that GetAll method pulls all items from db
+        [Fact]
+        public void TestGetAll_CheckOuts_ReturnsListOfCheckOuts()
+        {
+            //Arrange
+            DateTime dueDate = new DateTime(2017, 3, 15);
+            DateTime returnDate = new DateTime(2017, 3, 7);
+            CheckOut firstCheckOut = new CheckOut(dueDate, returnDate, 1, 1);
+            CheckOut secondCheckOut = new CheckOut(dueDate, returnDate, 2, 2);
+
+            //Act
+            firstCheckOut.Save();
+            secondCheckOut.Save();
+
+            //Assert
+            List<CheckOut> actualResult = CheckOut.GetAll();
+            List<CheckOut> expectedResult = new List<CheckOut>{firstCheckOut, secondCheckOut};
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        //Tests db-fetching specific item
+        [Fact]
+        public void TestFind_FindsCheckOutInDatabase()
+        {
+            //Arrange
+            DateTime dueDate = new DateTime(2017, 3, 15);
+            DateTime returnDate = new DateTime(2017, 3, 7);
+            CheckOut testCheckOut = new CheckOut(dueDate, returnDate, 1, 1);
+            testCheckOut.Save();
+
+            //Act
+            CheckOut foundCheckOut = CheckOut.Find(testCheckOut.GetId());
+
+            //Assert
+            Assert.Equal(testCheckOut, foundCheckOut);
+        }
+
+        [Fact]
+        public void Test_DeleteThisCheckOut_OneCheckOutDeletedFromDatabase()
+        {
+            //Arrange
+            DateTime dueDate = new DateTime(2017, 3, 15);
+            DateTime returnDate = new DateTime(2017, 3, 7);
+            CheckOut firstCheckOut = new CheckOut(dueDate, returnDate, 1, 1);
+            CheckOut secondCheckOut = new CheckOut(dueDate, returnDate, 2, 2);
+            firstCheckOut.Save();
+            secondCheckOut.Save();
+
+            //Act
+            firstCheckOut.DeleteCheckOut();
+            List<CheckOut> result = CheckOut.GetAll();
+            List<CheckOut> verify = new List<CheckOut>{secondCheckOut};
+
+            //Assert
+            Assert.Equal(verify, result);
+        }
+
+        //Delete everything between tests
+        public void Dispose()
+        {
+            CheckOut.DeleteAll();
+        }
+    }
+}
