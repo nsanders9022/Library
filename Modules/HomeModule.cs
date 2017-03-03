c4eab6293d8c9d2ceaac2f9c7b0c6bc79e467343
Nicole Sanders <nsanders9022@gmail.com>
2017-03-02 21:52:40 -0800

    add a form to enter a new book with corresponding autor and copy

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -5,40 +5,58 @@ using LibraryApp.Objects;
 
 namespace LibraryApp
 {
-  public class HomeModule : NancyModule
-  {
-    public HomeModule()
+    public class HomeModule : NancyModule
     {
-      Get["/"] = _ => {
-        Dictionary<object, object> model = new Dictionary<object, object>();
-        List<Checkout> allCheckouts = Checkout.GetAll();
-        foreach (Checkout checkout in allCheckouts)
+        public HomeModule()
         {
-          Patron thisPatron = Patron.Find(checkout.GetId());
-          model.Add(checkout, thisPatron);
+            Get["/"] = _ => {
+                Dictionary<object, object> model = new Dictionary<object, object>();
+                List<Checkout> allCheckouts = Checkout.GetAll();
+                foreach (Checkout checkout in allCheckouts)
+                {
+                    Patron thisPatron = Patron.Find(checkout.GetId());
+                    model.Add(checkout, thisPatron);
+                }
+                return View["index.cshtml", model];
+            };
+
+            Get["/books"] = _ => {
+                List<Book> allBooks = Book.GetAll();
+                return View["books.cshtml", allBooks];
+            };
+
+            Get["/patrons"] = _ => {
+                List<Patron> allPatrons = Patron.GetAll();
+                return View["patrons.cshtml", allPatrons];
+            };
+
+            Get["/authors"] = _ => {
+                List<Author> allAuthors = Author.GetAll();
+                return View["authors.cshtml", allAuthors];
+            };
+
+            Post["/add_book"] =  _ => {
+                Book newBook = new Book(Request.Form["title"]);
+                newBook.Save();
+
+                Author newAuthor = new Author(Request.Form["author-first"], Request.Form["author-last"]);
+                newAuthor.Save();
+                newBook.AddAuthor(newAuthor);
+
+                Copy newCopy = new Copy(newBook.GetId());
+                newCopy.Save();
+
+                newBook.AddCopy(Request.Form["num-copies"]);
+
+                List<Book> allBooks = Book.GetAll();
+
+                return View["books.cshtml", allBooks];
+            };
+            
+
+
         }
-        return View["index.cshtml", model];
-      };
-
-      Get["/books"] = _ => {
-        List<Book> allBooks = Book.GetAll();
-        return View["books.cshtml", allBooks];
-      };
-
-      Get["/patrons"] = _ => {
-        List<Patron> allPatrons = Patron.GetAll();
-        return View["patrons.cshtml", allPatrons];
-      };
-
-      Get["/authors"] = _ => {
-        List<Author> allAuthors = Author.GetAll();
-        return View["authors.cshtml", allAuthors];
-      };
-
-
 
     }
-
-  }
 
 }
